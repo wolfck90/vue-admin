248cec12c95a8e84cc833b714066b3f7b1a9ae77
xqs <xqs1004676115@gmail.com>
2021-02-19 15:44:30 +0800

    创建项目，配置全局样式，路由，element

---
+++ src/views/login/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="login">
+    这是主页
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+  </div>
+</template>
+
+<style lang="scss" scope>
+    #login {
+        background-color: #344a5f;
+        height: 100vh;
+    }
+</style>
